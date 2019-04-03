412780e7e6ce3ddbdbd980c1356e609b40821c5d
Donald Schartman <don.schartman@vu.com>
2019-04-03 11:22:29 -0500

    boom

--- env/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/dschartman/src/anacondacon.19/introduction_to_machine_learning_with_scikit-learn/env/lib"
-XML2_LIBS="-lxml2 -L/home/dschartman/src/anacondacon.19/introduction_to_machine_learning_with_scikit-learn/env/lib -lz -L/home/dschartman/src/anacondacon.19/introduction_to_machine_learning_with_scikit-learn/env/lib -llzma -lpthread -L/home/dschartman/src/anacondacon.19/introduction_to_machine_learning_with_scikit-learn/env/lib -L/home/dschartman/src/anacondacon.19/introduction_to_machine_learning_with_scikit-learn/env/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/home/dschartman/src/anacondacon.19/introduction_to_machine_learning_with_scikit-learn/env/include/libxml2 -I/home/dschartman/src/anacondacon.19/introduction_to_machine_learning_with_scikit-learn/env/include"
-MODULE_VERSION="xml2-2.9.9"
-
